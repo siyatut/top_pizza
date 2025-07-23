3ebc486504554392d38a29d9ac731a7ab0b5da6b
siyatut <anastasiya.tyutinova@gmail.com>
2025-07-23 17:04:07 +0300

    Initial Commit

---
+++ top_pizzaUITests/top_pizzaUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  top_pizzaUITestsLaunchTests.swift
+//  top_pizzaUITests
+//
+//  Created by Anastasia Tyutinova on 23/7/2568 BE.
+//
+
+import XCTest
+
+final class top_pizzaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
