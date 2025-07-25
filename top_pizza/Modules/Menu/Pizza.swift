19704a9e147a1446dd41b5e72808cf6623db8a64
siyatut <anastasiya.tyutinova@gmail.com>
2025-07-25 13:13:18 +0300

    Add Pizza model and PizzaResponse for API

--- top_pizza/Modules/Menu/Pizza.swift
+++ top_pizza/Modules/Menu/Pizza.swift
@@ -5,3 +5,20 @@
 //  Created by Anastasia Tyutinova on 25/7/2568 BE.
 //
 
+struct Pizza: Decodable {
+    let id: Int
+    let title: String
+    let description: String
+    let price: Int
+    let imageUrl: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, title, description, price
+        case imageUrl = "thumbnail"
+    }
+}
+
+struct PizzaResponse: Decodable {
+    let products: [Pizza]
+}
+
