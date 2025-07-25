98c67a4f24d32f39cd3fc9644fbc98dc7ca51e3f
siyatut <anastasiya.tyutinova@gmail.com>
2025-07-25 13:27:43 +0300

    Create MenuViewController with presenter binding

--- top_pizza/Modules/Menu/MenuViewController.swift
+++ top_pizza/Modules/Menu/MenuViewController.swift
@@ -5,3 +5,25 @@
 //  Created by Anastasia Tyutinova on 25/7/2568 BE.
 //
 
+import UIKit
+
+final class MenuViewController: UIViewController, MenuView {
+    
+    private var presenter: MenuPresenter!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        presenter = MenuPresenter(view: self)
+        presenter.loadMenu()
+    }
+
+    func display(pizzas: [Pizza]) {
+        print("Got \(pizzas.count) pizzas")
+    }
+
+    func display(error: String) {
+        print("Error loading pizzas: \(error)")
+    }
+}
+
